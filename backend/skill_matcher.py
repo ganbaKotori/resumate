c4c75911243d708c87a8b37acf0e2b4cedec312b
David <garzadavid26@yahoo.com>
2021-03-27 20:41:09 -0700

    added skills matcher to backend
    first attempt at comparing skills from user input with the skills from job qualifications

---
+++ backend/skill_matcher.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+url = 'https://www.linkedin.com/jobs/view/2442922626/?eBP=NotAvailableFromMidTier&recommendedFlavor=ACTIVELY_HIRING_COMPANY&refId=6uFwmmDAwclD46luXJB1Uw%3D%3D&trackingId=uz6y3Dr%2FHJAatiKQEUQYuA%3D%3D&trk=flagship3_search_srp_jobs'
+
+def retrieve_job_qualifications(URL) :
+    '''Function that takes in a webpage and retrieves the html via beautiful soup'''
+    webpage = requests.get(URL)
+    soup = bs(webpage.content, 'html.parser')
+
+    return soup
+
+def compare_skills(soup, skills) :
+    '''Function that checks whether or not the user has the skills the job requires'''
+    matched_skills = []
+
+    for skill in skills :
+        if skill.lower() in soup.text.lower() :
+            matched_skills.append(skill)
+
+    if len(matched_skills) == 0 :
+        print('You do not meet the qualifications for this job')
+    else :
+        print('These skills match the jobs qualifications: ')
+        for skill in matched_skills :
+            print('- ' + skill)
+
+test = retrieve_job_qualifications(url)
+
+skills = ['java', 'databases', 'c++', 'c#', 'visual studio', 'zynq', 'linux', 'aws', 'angular', 'html', 'javascript']
+
+compare_skills(test, skills)
